5ad0ac32778c46c7cf7e51e790b4271c91bb9115
spaceinvadder <standardbearerofhay@yandex.ru>
2025-02-17 21:07:58 +0300

    начало

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+class Spaceship {
+  private:
+    unsigned int hp; // здоровье
+    unsigned int speed; // скорость
+    unsigned int armor; // броня
+}; //корабль
+class Enemy {}; // враги
+class Planet {}; // планеты
+class Weapon {}; // оружие
+class Mission {}; // миссии
+class Game {};  // логика игры
+
