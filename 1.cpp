586548d50afd376ee7003ceeb8450d1f0a1c5bc7
spaceinvadder <standardbearerofhay@yandex.ru>
2025-02-24 17:42:04 +0300

    добавлено начало логики игры

--- 1.cpp
+++ 1.cpp
@@ -10,5 +10,19 @@ class Enemy {}; // враги
 class Planet {}; // планеты
 class Weapon {}; // оружие
 class Mission {}; // миссии
-class Game {};  // логика игры
+class Game {
+ private:
+  Spaceship ship;
+  Enemy enemies[unsigned int number_of_enemies];
+  Mission mission[unsigned int number_of_missions];
+  bool game_over;
+ public:
+   Game(): game_over(false) {};
+   void game_start() {
+     while (!game_over) {
+
+     }
+   };
+   ~Game() {};
+};  // логика игры
 
