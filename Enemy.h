f79929341b25cdf6c13ec6efc9e5e5574d290c88
spaceinvadder <standardbearerofhay@yandex.ru>
2025-04-14 17:51:25 +0300

    добавлена логика скорости

--- Enemy.h
+++ Enemy.h
@@ -8,12 +8,21 @@ public:
     virtual void update(float dt) = 0;
     virtual void draw(sf::RenderWindow& window) const = 0;
     virtual const sf::FloatRect getBounds() const = 0;
-    virtual bool isAlive() const = 0;
+    
+    virtual bool isAlive() const {
+      return alive;
+    }
+
     virtual int getScoreValue() const = 0;
     virtual void takeDamage() = 0;
+
+    void setVelocity(float vx, float vy) {
+        velocity.x = vx;
+        velocity.y = vy;
+    }
 
 protected:
     sf::Vector2f position;
     sf::Vector2f velocity;
-    bool alive;
+    bool alive = true;
 };
